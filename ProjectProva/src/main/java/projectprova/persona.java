a19119e9666a56adc69663a392e555655a1103a5
FossiMirco02 <64960209+FossiMirco02@users.noreply.github.com>
2020-12-20 18:15:59 +0100

    Add files via upload

---
+++ ProjectProva/src/main/java/projectprova/persona.java
@@ -0,0 +1,34 @@
+package projectprova;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Famiglia
+ */
+public class persona 
+{
+    String nome;
+    String cognome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+    
+}
